a27da7195eeeaaeb1d6a1af81c1b358623a9cedf
Jessica Yin <jyin978@hotmail.com>
2020-04-01 16:29:27 -0700

    added another aexercise
    anotherjava exercise on the use of \"

--- MyFirstProgram.java
+++ MyFirstProgram.java
@@ -6,3 +6,15 @@ public class MyFirstProgram {
         System.out.println("yay!");
     }
 }
+
+class MuchBetter {
+    public static void main(final String[] args){
+    System.out.println("A \"quoted\" String is");
+    System.out.println("'much' better if you learn");
+    System.out.println("the rules of \"escape sequences.\" ");
+    System.out.println();
+    System.out.println("Also, \"\" represents an empty String.");
+    System.out.println("Don't forget: Use \"\'\" instead of \" !");
+    System.out.println(" '' is not the same as \"\" ");
+    }
+}
