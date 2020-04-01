92b2ff66c393ea762884aa24e2c08a4a43c67267
Jess Y <31145424+jcat1504@users.noreply.github.com>
2020-04-01 13:39:28 -0700

    Add files via upload

---
+++ MyFirstProgram.java
@@ -0,0 +1,8 @@
+public class MyFirstProgram {
+    public static void main(final String[] args) {
+        System.out.println("Meowdy, world!");
+        System.out.println();
+        System.out.println("This is my very first program!");
+        System.out.println("yay!");
+    }
+}
