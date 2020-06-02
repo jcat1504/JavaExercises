3110dea5fae6a099bec8bd302f1ff4d4c2f56650
Jessica Yin <jyin978@hotmail.com>
2020-06-01 21:07:40 -0700

    Create polymorphism2.java

---
+++ OOP Concepts/polymorphism2.java
@@ -0,0 +1,7 @@
+//dynmamic method dispatch is the mechanism by which a call to an overriden method is resolved at run time, rather than compile time
+//when an overriden method is called through a superclass reference, Java determines which version(superclass/subclasses) of
+//that method is to be executed based upon the type of the object being referred to at the time the call occurs.
+    //thus this determination is made at run time
+//at run time, it depends onthe type of the object being referred to(not the type reference variable) that determines
+//which version of an overriden method will be executed
+//a superclass reference variable can refer to a subclass object. this is known as upcasting.
