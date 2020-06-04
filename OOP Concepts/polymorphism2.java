1067e5d25a6277e5029bbd12db2a251bfec60e11
Jessica Yin <jyin978@hotmail.com>
2020-06-03 17:25:28 -0700

    Update polymorphism2.java

--- OOP Concepts/polymorphism2.java
+++ OOP Concepts/polymorphism2.java
@@ -5,3 +5,10 @@
 //at run time, it depends onthe type of the object being referred to(not the type reference variable) that determines
 //which version of an overriden method will be executed
 //a superclass reference variable can refer to a subclass object. this is known as upcasting.
+
+//upcasting: 
+SuperClass obj = new SubClass 
+
+//therefore if a superclass contains a method that is overriden by a subclass, then when different types of objects are referred 
+//through a superclass reference variable, different versions of the method are executed.
+
