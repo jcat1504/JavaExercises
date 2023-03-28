42412cc4ae453e467dcf47493f6382031b56b1a6
Jess Y <jyin978@hotmail.com>
2023-03-27 20:56:12 -0400

    Lesson 1 - learning classes and methods

---
+++ CodeGym/lesson-1.java
@@ -0,0 +1,22 @@
+// Java commands must be inside functions, and functions must be inside classes
+//in Java, functions are also called methods 
+//Method - is part of a class. A class is divided into methods and methods contain commands
+// Java programs are made up of classes - classes contain methods - methods contain commands 
+
+//classes can contain multiple methods too
+
+public class House {
+    int a;
+    int b;
+
+    public static void main (String[] args) {
+        System.out.print(1);
+    }
+
+    public static double pi () {
+        return 3.14;
+    }
+}
+
+//Minimal program must consist of at least one class and one method(function) that marks the programs
+//starting point and must be named main! it is immutable
