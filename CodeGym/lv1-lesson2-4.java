a89be2c143648c219a0a50061ad0ea74d485e4c2
Jess Y <jyin978@hotmail.com>
2023-03-28 12:58:00 -0400

    "assigning variables lesson"

---
+++ CodeGym/lv1-lesson2-4.java
@@ -0,0 +1,31 @@
+//A method body consists of commands
+//println() does not display text on a new line. INstead, it displays text on current line - the next
+//line will be displayed at the beginning of a new line!
+
+//Variables are things for storing data. Think of a variable as a box.
+//Variable has 3 properties -- name, type, value
+// name: variable
+//type: values/data...int, fractional number, string, etc
+//value: some object or data stored in variable
+
+int a;
+String s;
+double c;
+
+int a = 5;
+int b = 6;
+
+//YOU CANNOT create two variables with same name, but in different methods you can :)
+//compact code vs long code!
+int d = c + 1; 
+
+int d;
+d = c + 1;
+
+String s = "I'm Jess";
+String s;
+s = "I'm Jess";
+
+int x = 333;
+String s = "Amigo" + x;
+//Amigo333
