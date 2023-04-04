3e1e58bcee8622a24433e215aad3614a6d4a7b93
Jess Y <jyin978@hotmail.com>
2023-04-03 20:52:40 -0400

    GettersandSetters

--- Lv2-classesAndObjects.java
+++ Lv2-classesAndObjects.java
@@ -49,3 +49,52 @@ golf.increaseSpeed(30);
 // These modifiers determine the accessibility of classes, methods, and variables to other parts of the program. 
 
 // **Classes usually have public modifiers but we tend to keep our fields private.**
+public class Car {
+    private String type;
+    // ...
+
+    public Car(String type, String model, String color) {
+       // ...
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    // ...
+}
+
+// **Our class is marked *public*, which means we can use it in any package.** Also, the constructor is *public*, which means we can create an object from this class inside any other object.
+
+// **Our fields are marked *private*, which means they're not accessible from our object directly**, but we provide access to them through getters and setters.
+
+// Setters: updates value of variable
+
+// Getters: reads value of variable
+
+// The *type* and *model* fields do not have getters and setters, because they hold internal data of our objects. We can define them only through the constructor during initialization.
+
+// Furthermore, the *color* can be accessed and changed, whereas *speed* can only be accessed, but not changed. We enforced speed adjustments through specialized *public* methods *increaseSpeed()* and *decreaseSpeed()*.
+
+// In other words, **we use access control to encapsulate the state of the object.**
+
+// ## **Getters and Setters**
+
+// - Getter: (accessor method) is used to retrieve the value of a private variable in a class. It is a public method that returns the value of the private variable it is associated with.
+//     - By convention, the name of getter method should start with “get” followed by name of private variable
+
+public class Person {
+	private String name;
+ 
+	public String getName() {
+	   return name;
+	}
+ }
