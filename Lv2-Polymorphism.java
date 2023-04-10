4902ad47e1d8ccda01e2458d38f3c95725c9b2ac
Jess Y <jyin978@hotmail.com>
2023-04-10 15:55:10 -0400

    Polymorphism examples

--- Lv2-Polymorphism.java
+++ Lv2-Polymorphism.java
@@ -29,3 +29,22 @@ shapes.add(squareShape);
 for (Shape shape : shapes) {
 		System.out.println(shape.name());
 }
+
+public interface HasColor {
+	String getColor();
+}
+
+public interface Box extends HasColor {
+	int getHeight();
+}
+
+//In the example above, Box inherits from HasColor using the keyword extends. By doing so, the Box interface inherits getColor. As a result, the Box interface now has two methods: getColor and getHeight.
+
+//Abstract class implementing an interface
+public interface Transform {
+	void transform();
+	default void printSpecs() {
+		System.out.println("Transform Specification");
+	}
+}
+public abstract class Vehicle implements Transform {}
