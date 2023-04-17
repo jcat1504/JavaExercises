95d0fe4f7a8871eacb93825b5d8350271b2e0b38
Jess Y <jyin978@hotmail.com>
2023-04-17 19:12:05 -0400

    Super keyword

--- superKeyword.java
+++ superKeyword.java
@@ -6,3 +6,42 @@ public class SuperSub extends SuperBase {
 }
 
 Supersub child = new SuperSub("message from the child class!");
+
+//accessing parent class variables
+public class SuperBase {
+	String message = "super class";
+
+//default constructor
+
+	public SuperBase(String message) {
+		this.message = message;
+	}
+}
+//now we create child class with var of same name
+public class SuperSub extends SuperBase {
+	String message = "Child class";
+	public void getParentMessage() {
+		System.out.println(super.message);
+	}
+}
+
+public class SuperBase {
+	String message = "super class";
+
+	public void printMessage() {
+		System.out.println(message);
+	}
+}
+public class SuperSub extends SuperBase {
+	String message = "child class";
+
+	public SuperSub() {
+		super.printMessage();//super class
+		printMessage();//child class
+		}
+
+public void printMessage() {
+	System.out.println(message);
+	}
+}
+//super keyword with method overriding
