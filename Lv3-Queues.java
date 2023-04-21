4f01abdcccd632e40220c432719f0505de6359c5
Jess Y <jyin978@hotmail.com>
2023-04-20 20:32:56 -0400

    priority queues

--- Lv3-Queues.java
+++ Lv3-Queues.java
@@ -10,3 +10,42 @@ Queue<String> queue = new LinkedList<String>();
 
 System.out.println(queue);
 // [Karen, Chad, Steve, Harold]
+
+//PRIORITY QUEUES
+public static void main(String args[]) {
+	Queue<Double> queue = new PriorityQueue<>();
+
+//if you want reverse order:
+//Queue<Double> queue = new PriorityQueue<>(Collections.reverseOrder());
+
+		queue.offer(3.0);
+		queue.offer(2.5);
+		queue.offer(4.0);
+		queue.offer(1.5);
+		queue.offer(2.0);
+
+	while(!queue.isEmpty()) {
+		System.out.println(queue.poll());
+		}
+	}
+
+//This will arrange elements in numerical order
+public static void main(String args[]) {
+	Queue<String> queue = new PriorityQueue<>(); 
+
+//if you want reverse order:
+//Queue<Double> queue = new PriorityQueue<>(Collections.reverseOrder());
+
+		queue.offer("A");
+		queue.offer("C");
+		queue.offer("B");
+		queue.offer("F");
+		queue.offer("D");
+		
+		while(!queue.isEmpty()) {
+			System.out.println(queue.poll());
+		}
+	}
+
+
+//will organize alphabetical order
