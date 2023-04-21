ba204d4cf410120753ea2f8d267d3e00ebd50fa1
Jess Y <jyin978@hotmail.com>
2023-04-20 20:30:38 -0400

    arrays

---
+++ Lv.3-Arrays.java
@@ -0,0 +1,8 @@
+int sampleArray[5] = {2, 4, 6, 8, 100}
+//sampleArray = 2, 4, 6, 8, 100
+
+//if you want to change a certain element
+sampleArray[0] = 20;
+sampleArray[1] = -5;
+//sampleArray = 20, -5, 6, 8, 100
+
