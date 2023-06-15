af8fedac5b368d1aafb66305eae87bc172358e41
Jess Y <jyin978@hotmail.com>
2023-06-15 16:21:22 -0400

    recursion method in max depth of binary tree

--- HackerRankAndLeetcode/groupAnagrams.java
+++ HackerRankAndLeetcode/groupAnagrams.java
@@ -17,3 +17,4 @@ class Solution {
         return new ArrayList(map.values());
     }
 }
+
--- HackerRankAndLeetcode/maxDepthOfBinaryTree.java
+++ HackerRankAndLeetcode/maxDepthOfBinaryTree.java
@@ -34,3 +34,9 @@ class Solution {
         return height;  // return height
     }
 }
+
+// recursion method
+public int maxDepth(TreeNode root) {
+    if (root == null) return 0;
+    return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+}
