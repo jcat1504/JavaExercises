6c728c9ad89573f44f20a625a972413130fffac8
Jess Y <jyin978@hotmail.com>
2023-07-10 15:55:19 -0400

    sameTree solution #2

--- HackerRankAndLeetcode/sameTree.java
+++ HackerRankAndLeetcode/sameTree.java
@@ -5,3 +5,23 @@ public boolean isSameTree(TreeNode p, TreeNode q) {
     if (p.val != q.val) return false;
     return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 }
+
+public boolean isSameTree(TreeNode p, TreeNode q) {        
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(p);
+        queue.add(q);
+        while(!queue.isEmpty()){
+            TreeNode f = queue.poll();
+            TreeNode s = queue.poll();
+            if(f == null && s == null){
+                continue;
+            }else if(f == null || s == null || f.val != s.val){
+                return false;
+            }
+            queue.add(f.left);
+            queue.add(s.left);
+            queue.add(f.right);
+            queue.add(s.right);
+        }
+        return true;
+    }
