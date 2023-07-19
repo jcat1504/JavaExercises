15dede396e3cee65b37e4cb0190a9afef6f949e8
Jess Y <jyin978@hotmail.com>
2023-07-19 10:36:04 -0400

    solution to clonegraph with dfs recursively.

--- HackerRankAndLeetcode/cloneGraph.java
+++ HackerRankAndLeetcode/cloneGraph.java
@@ -1,3 +1,16 @@
-public class cloneGraph {
-    
-}
+import org.w3c.dom.Node;
+
+class Solution {
+    Node[] visited;
+    public Node cloneGraph(Node node) {
+        if(node == null) return null;
+        visited = new Node[101];
+        return DFS(node);
+    }
+    public Node DFS(Node node){
+        if(visited[node.val] != null) return visited[node.val];
+        visited[node.val] = new Node(node.val);
+        for(Node n: node.neighbors) visited[node.val].neighbors.add(DFS(n));
+        return visited[node.val];
+    }
+}  
