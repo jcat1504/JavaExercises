eecb9e8a1ea2711115362eef74bc144df1e79bf8
Jess Y <jyin978@hotmail.com>
2023-08-17 10:57:19 -0400

    climbing stairs dp solution

---
+++ HackerRankAndLeetcode/climbingStairs.java
@@ -0,0 +1,19 @@
+public int climbStairs(int n) {
+    if (n<= 1){
+        return 1;
+    }
+
+    int[] dp = new int[n+1];
+    dp[1] = 1; //since there's only one way to reach each step
+    d[2] = 2;//there are two ways to reach second step
+
+    for(int i = 3; i<=n; i++) {
+        //adds number of ways to reach previous step
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+    return dp[n]
+    }
+}
+
+//loop starts from 3 because we already calculated and stored base cases for first 
+// 2 steps. from third step onwards, we will calculate
