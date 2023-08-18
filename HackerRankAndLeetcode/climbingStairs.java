41b8be5e69fcb46ea9454775a04ecd57d89bb85f
Jess Y <jyin978@hotmail.com>
2023-08-18 12:23:15 -0400

    climbing stairs solution iteratively

--- HackerRankAndLeetcode/climbingStairs.java
+++ HackerRankAndLeetcode/climbingStairs.java
@@ -17,3 +17,14 @@ public int climbStairs(int n) {
 
 //loop starts from 3 because we already calculated and stored base cases for first 
 // 2 steps. from third step onwards, we will calculate
+
+public int climbStairs(int n) {
+    if(n<=1) return 1;
+    int prev1 = 1; int prev2 = 2;
+    for(int i=3; i<=n;i++) {
+        int nextVal = prev1 + prev2;
+        prev1 = prev2;
+        prev2 = nextVal;
+    }
+    return nextVal;
+}
