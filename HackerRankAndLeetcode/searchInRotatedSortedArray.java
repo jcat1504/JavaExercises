7c532f1459773f65041c82ae58d1c28f70735016
Jess Y <jyin978@hotmail.com>
2023-08-29 11:14:33 -0400

    comments in code

--- HackerRankAndLeetcode/searchInRotatedSortedArray.java
+++ HackerRankAndLeetcode/searchInRotatedSortedArray.java
@@ -1,3 +1,4 @@
+class Solution {
     public int search(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
@@ -6,24 +7,24 @@
             int mid = (left + right) / 2;
             
             if (nums[mid] == target) {
-                return mid; 
+                return mid; // If the middle element is the target, return its index
             }
             
             if (nums[left] <= nums[mid]) {
                 if (nums[left] <= target && target < nums[mid]) {
-                    right = mid - 1;
+                    right = mid - 1; // Search in the left half of the current sorted region
                 } else {
-                    left = mid + 1;
+                    left = mid + 1; // Search in the right half of the current sorted region
                 }
             } else {
                 if (nums[mid] < target && target <= nums[right]) {
-                    left = mid + 1;
+                    left = mid + 1; // Search in the right half of the current sorted region
                 } else {
-                    right = mid - 1;
+                    right = mid - 1; // Search in the left half of the current sorted region
                 }
             }
         }
         
-        return -1; 
+        return -1; // Target not found in the array
     }
-
+}
