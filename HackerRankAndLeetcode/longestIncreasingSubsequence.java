9cd179a06df393b32f187529979f394c52f60c5d
Jess Y <jyin978@hotmail.com>
2023-08-31 10:59:13 -0400

    memoization solution

--- HackerRankAndLeetcode/longestIncreasingSubsequence.java
+++ HackerRankAndLeetcode/longestIncreasingSubsequence.java
@@ -20,3 +20,39 @@ class Solution {
                 return max;
             }
         }
+    
+
+class Solution {
+    public static int[] listLengths;
+
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        listLengths = new int[n];
+        for (int i = 0; i < n; i++) {
+            listLengths[i] = -1;
+        }
+
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            max = Math.max(max, lis(nums, i));
+        }
+        return max;
+    }
+
+    private static int lis(int[] nums, int i) {
+        if (listLengths[i] != -1) {
+            return listLengths[i];
+        }
+
+        int max = 1;
+        for (int j = 0; j < i; j++) {
+            if (nums[i] > nums[j] && max < listLengths[j] + 1) {
+                max = listLengths[j] + 1;
+            }
+        }
+
+        listLengths[i] = max;
+        return max;
+    }
+}
+
