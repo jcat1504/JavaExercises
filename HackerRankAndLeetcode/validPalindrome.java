d2e386deeb376dec2eafc1d3efbd8c2edffa6000
Jess Y <jyin978@hotmail.com>
2023-09-15 12:44:14 -0400

    probably most straightforward solution

--- HackerRankAndLeetcode/validPalindrome.java
+++ HackerRankAndLeetcode/validPalindrome.java
@@ -47,3 +47,17 @@ class Solution {
         
         return true;
     }
+
+        public boolean isPalindrome(String s) {
+        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        int left=0; int right=s.length()-1;
+        while(left<right) {
+            if(s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
