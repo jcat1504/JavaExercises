e6deecdb314574efffb8fbfd86d7beb7e4645d9e
Jess Y <jyin978@hotmail.com>
2023-09-28 11:23:32 -0400

    second solution

--- HackerRankAndLeetcode/numberOfIslands.java
+++ HackerRankAndLeetcode/numberOfIslands.java
@@ -26,3 +26,31 @@ private void DFSMarking(char[][] grid, int i, int j) {
     DFSMarking(grid, i, j + 1);
     DFSMarking(grid, i, j - 1);
 }
+
+//better solution
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) return 0;
+        int numRows = grid.length; int numCols = grid[0].length;
+        int count = 0;
+        for(int row = 0; row < numRows; row++) {
+            for(int col = 0; col < numCols; col++) {
+                if(grid[row][col]=='1') {
+                    count++;
+                    dfs(grid,row,col);
+                }
+            }
+        }
+        return count;
+    }
+
+    public void dfs(char[][] grid, int row, int col) {
+        int numRows = grid.length; int numCols = grid[0].length;
+        if(row<0 || row>=numRows || col<0 || col>=numCols || grid[row][col] =='0') {
+            return;
+        }
+        grid[row][col] = '0';
+        dfs(grid, row-1, col);
+        dfs(grid, row+1, col);
+        dfs(grid, row, col-1);
+        dfs(grid, row, col+1);
+    }
