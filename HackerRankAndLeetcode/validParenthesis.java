ea3d4af94e4273f0633e0b9e496742163042f75c
jcat1504 <jyin978@hotmail.com>
2023-12-04 14:53:22 -0500

    second stack solution

--- HackerRankAndLeetcode/validParenthesis.java
+++ HackerRankAndLeetcode/validParenthesis.java
@@ -30,3 +30,24 @@ class Solution {
         return false;
     }
 }
+
+//second solution
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for(char c: s.toCharArray()) {
+            if(c == '(') {
+                stack.push(')');
+            }
+            else if (c == '{') {
+                stack.push('}');
+            }
+            else if (c == '[') {
+                stack.push(']');
+            }
+            else if (stack.isEmpty() || stack.pop() != c) {
+                return false;
+            }
+        }     
+        return stack.isEmpty();
+    }
+}
