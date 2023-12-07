844a04d442ded5983264fbdf06df4b176b5483b5
jcat1504 <jyin978@hotmail.com>
2023-12-07 12:36:58 -0500

    bottom up dp approach

--- HackerRankAndLeetcode/maxSubArray.java
+++ HackerRankAndLeetcode/maxSubArray.java
@@ -13,3 +13,25 @@ class Solution {
         return max;
     }
 }
+
+//second solution
+public static int maxSubArray(int[] nums) {
+    int n = nums.length;
+    int[] dp = new int[n];
+
+    // Base case
+    dp[0] = nums[0];
+
+    // Calculate maximum subarray sum at each index
+    for (int i = 1; i < n; i++) {
+      dp[i] = Math.max(nums[i], nums[i] + dp[i - 1]);
+    }
+
+    // Find the maximum sum in the 'dp' array
+    int maxSum = Integer.MIN_VALUE;
+    for (int value : dp) {
+      maxSum = Math.max(maxSum, value);
+    }
+
+    return maxSum;
+}
