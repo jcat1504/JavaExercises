2ef2c27ec87dcea3961cb011340c233b01b56253
Jess Y <jyin978@gmail.com>
2023-12-14 20:18:50 -0500

    solution to ransom note with hashmap

---
+++ HackerRankAndLeetcode/ransomNote.java
@@ -0,0 +1,13 @@
+public boolean canConstruct(String ransomNote, String magazine) {
+        Map<Character, Integer> magazineMap = new HashMap<>();
+        for(char c : magazine.toCharArray()) {
+            magazineMap.put(c, magazineMap.getOrDefault(c,0) + 1);
+        }
+        for(char c : ransomNote.toCharArray()) {
+            if(!magazineMap.containsKey(c) || magazineMap.get(c) == 0){
+            return false;
+        }
+        magazineMap.put(c, magazineMap.get(c) -1);
+    }
+    return true;
+}
