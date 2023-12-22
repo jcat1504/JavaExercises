098795825141505c861752c0798b6a2b31d73c49
Jess Y <jyin978@gmail.com>
2023-12-21 19:14:03 -0500

    less optimal solution

--- HackerRankAndLeetcode/productOfArrayExceptSelf.java
+++ HackerRankAndLeetcode/productOfArrayExceptSelf.java
@@ -20,3 +20,22 @@ class Solution {
         return ans;
     }
 }
+
+//diff less optimal approach
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int ans[] = new int[n];
+        
+        for(int i = 0; i < n; i++) {
+            int pro = 1;
+            for(int j = 0; j < n; j++) {
+                if(i == j) continue;
+                pro *= nums[j];
+            }
+            ans[i] = pro;
+        }
+        
+        return ans;
+    }
+}
