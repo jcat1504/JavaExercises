5596b20a5ae9b968f24803caff8e54d94dc41be0
Jess Y <jyin978@gmail.com>
2024-01-08 19:58:03 -0500

    solution to valid bst and reverse linked list

--- HackerRankAndLeetcode/validateBST.java
+++ HackerRankAndLeetcode/validateBST.java
@@ -20,19 +20,16 @@ import javax.swing.tree.TreeNode;
 class Solution {
 
     public boolean isValidBST(TreeNode root) {
-        if (root == null) return true;
         return dfs(root, null, null);
     }
 
     private boolean dfs(TreeNode root, Integer min, Integer max) {
-        if (root == null) return true;
-
-        if (
-            (min != null && root.val <= min) || max != null && root.val >= max
-        ) {
+        if (root == null) {
+            return true;
+        }
+        if((min != null && root.val <= min) || (max != null && root.val >= max)) {
             return false;
         }
-
         return dfs(root.left, min, root.val) && dfs(root.right, root.val, max);
     }
 }
