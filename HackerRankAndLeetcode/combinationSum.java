750d33d0d2daaafa1663b1c33db62e91e4498415
Jess Y <jyin978@hotmail.com>
2024-01-12 20:16:06 -0500

    second solution with dp

--- HackerRankAndLeetcode/combinationSum.java
+++ HackerRankAndLeetcode/combinationSum.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 class Solution {
@@ -24,4 +25,33 @@ class Solution {
             //backtracking - remove last candidate so we can move on and search for the next
         }
     }
+}
+
+//second solution with dp
+
+public List<List<Integer>> combinationSum(int[] candidates, int target) {
+    Arrays.sort(candidates);
+    LList<List<Integer>>[] dp = new ArrayList[target + 1];
+    
+    for (int t = 0; t <= target; t++) {
+        dp[t] = new ArrayList();
+        
+        List<List<Integer>> combList = new ArrayList();
+        
+        for (int j = 0; j < candidates.length && candidates[j] <= t; j++) {
+            if (candidates[j] == t) {
+                combList.add(Arrays.asList(candidates[j])); // itself can form a list
+            } else {
+                for (List<Integer> prevlist : dp[t - candidates[j]]) { 
+                    if (candidates[j] >= prevlist.get(prevlist.size() - 1)) {
+                        List temp = new ArrayList(prevlist); 
+                        temp.add(candidates[j]); 
+                        combList.add(temp);
+                    }
+                }
+            }
+        }
+        dp[t] = combList;
+    }
+    return dp[target];
 }
